2d470f91ebd441f427b8a5cffcaa55eb35e03f86
NelsonOrtiz94 <ortnn990@gmail.com>
2025-08-11 10:41:54 -0500

    Se estructura ProductoRepository

---
+++ src/main/java/com/lta/cursoapis/repository/ProductoRepository.java
@@ -0,0 +1,18 @@
+package com.lta.cursoapis.repository;
+
+import com.lta.cursoapis.entity.EstadoProducto;
+import com.lta.cursoapis.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+
+    Optional<Producto> findByIdProducto(Long idProducto);
+    Optional<Producto> findByNombreProducto(String nombreProducto);
+
+    List<Producto> findByEstado(EstadoProducto estadoProducto);
+}
